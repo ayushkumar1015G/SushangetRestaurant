ce960854c230d8805a4d75a9fd340b357fbae590
Ayush Kumar <ashu28042004@gmail.com>
2024-12-24 00:13:46 +0530

    try payment gateway
    just first try to convert stripe to razorpay payment gateway

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,7 +1,10 @@
+import Razorpay from "razorpay";
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
-import Stripe from "stripe";
+import crypto from "crypto";
+import razorpay from "razorpay";
 
+const {RAZORPAY_ID_KEY , RAZORPAY_SECRET_KEY} =process.env
 // const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order from frontend
@@ -22,57 +25,95 @@ const placeOrder = async (req, res) => {
     const line_items = req.body.items.map((item) => {
       return {
         price_data: {
-          currency: "INR",
+          currency: "inr",
           product_data: {
             name: item.name,
           },
-          unit_amount: item.price * 80 * 100,
+          unit_amount: item.price * 100,
         },
         quantity: item.quantity,
       };
     });
 
-    line_items.push({
-      price_data: {
-        currency: "inr",
-        product_data: {
-          name: "Delivery Charges",
-        },
-        unit_amount: 2 * 100 * 80,
-      },
-      quantity: 1,
-    });
+          //Delivery charges add krne ho toh 
+
+    // line_items.push({
+    //   price_data: {
+    //     currency: "inr",
+    //     product_data: {
+    //       name: "Delivery Charges",
+    //     },
+    //     unit_amount: 2 * 100,
+    //   },
+    //   quantity: 1,
+    // });
 
     // const session = await stripe.checkout.sessions.create({
     //     line_items:line_items,
     //     mode: 'payment',
-    // success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-    console.log("success_url");
+    //     success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
     //     cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
     // })
 
-    res.json({success: true, session_url: session.url});
-    // res.json({success: true, session_url: "http://localhost:5173/myorders"});
+    
+    const instance = new Razorpay({
+      key_id: RAZORPAY_ID_KEY,
+      key_secret: RAZORPAY_SECRET_KEY,
+    });
+
+    const totalAmount = line_items.reduce((acc, item) => {
+      return acc + (item.price_data.unit_amount) * item.quantity; 
+    }, 0);
+
+    const options = {
+      amount: totalAmount,
+      currency: "INR",
+      receipt: newOrder._id,
+    }
+
+    instance.orders.create(options, (error,order)=>{
+      if(error){
+        console.log(error);
+          return res.status(500).json({message:"Something Went Wrong"});
+        }
+        else{
+          res.status(200).json({data:order, success: true, session_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`});
+        }
+    })
+    // res.json({success: true, session_url: success_url"});
   } catch (error) {
     console.log(error);
-    res.json({success: false, error: "Error"});
+    res.json({success: false, error: "Error in placing order"});
   }
 };
 
 const verifyOrder = async (req, res) => {
-  const {orderId, success} = req.body;
-
+  
   try {
-    if (success == "true") {
+    const {razorpay_order_id,razorpay_payment_id,razorpay_signature,orderId, success} = req.body;
+    const sign = razorpay_order_id + "|" + razorpay_payment_id;
+		const expectedSign = crypto
+			.createHmac("sha256", process.env.KEY_SECRET)
+			.update(sign.toString())
+			.digest("hex");
+
+		if (razorpay_signature === expectedSign) {
       await orderId.findByIdAndUpdate(orderId, {payment: true});
-      res, json({success: true, message: "Paid"});
-    } else {
-      await orderModel.findByIdAndDelete(orderId);
-      res.json({success: false, message: "Payment Unsuccessful!!"});
-    }
+			return res.status(200).json({ message: "Payment verified successfully" });
+		} else {
+			return res.status(400).json({ message: "Invalid signature sent!" });
+		}
+
+    // if (success == "true") {
+    //   await orderId.findByIdAndUpdate(orderId, {payment: true});
+    //   res, json({success: true, message: "Paid"});
+    // } else {
+    //   await orderModel.findByIdAndDelete(orderId);
+    //   res.json({success: false, message: "Payment Unsuccessful!!"});
+    // }
   } catch (error) {
     console.log(error);
-    res.json({success: false, message: "Error"});
+    res.json({success: false, message: "Error in verifying the payment"});
   }
 };
 
