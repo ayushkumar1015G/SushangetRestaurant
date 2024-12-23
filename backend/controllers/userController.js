f3b6207a9dd5dcdbd4d8c87e1d103a0b5b024682
Ayush Kumar <ashu28042004@gmail.com>
2024-12-23 21:42:09 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -49,7 +49,7 @@ const registerUser = async (req, res) => {
     if (password.length < 8) {
       return res.json({
         success: false,
-        message: "Please enter a strong password",
+        message: "Please enter a strong password(must be >= 8 characters)",
       });
     }
 
